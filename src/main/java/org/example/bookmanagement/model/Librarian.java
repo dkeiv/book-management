0e8d75be862f8345e687c6a8e01b82e744f2e429
keiv <vudangkhoi1994@gmail.com>
2024-08-05 09:09:45 +0700

    Update Librarian.java

--- src/main/java/org/example/bookmanagement/model/Librarian.java
+++ src/main/java/org/example/bookmanagement/model/Librarian.java
@@ -1,2 +1,56 @@
-package org.example.bookmanagement.model;public class Librarian {
+package org.example.bookmanagement.model;
+
+public class Librarian {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    // for update
+    public Librarian() {
+    }
+
+    public Librarian(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Librarian(int id, String name, String email, String password) {
+        this(name, email, password);
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+//    private String hashPassword(String password) {
+//        String hashedPassword = "";
+//        return hashedPassword;
+//    }
 }
